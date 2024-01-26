83d28bb9b1f5895329b111c9fa25056bb07f89f4
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-01-26 15:49:56 +0200

    feat: Shake sign in button if login fails

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -150,7 +150,7 @@ extension LoginViewController {
             configureView(withMessage: "Username / password cannot be blank")
             return
         }
-        if username == "A" && password == "A" {
+        if username == "Flyn" && password == "Welcome" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
@@ -160,8 +160,20 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMeassageLabel.isHidden = false
         errorMeassageLabel.text = message
+        shakeButton()
+    }
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
+
 
 extension LoginViewController {
     
