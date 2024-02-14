177f2c9a726efefbfe30f50e9e45770ab449822e
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-02-14 14:02:46 +0200

    feat: Add layout for password Textfield

--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
+    let stackView = UIStackView()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            style()
+            layout()
+        }
     }
+    
+extension ViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(newPasswordTextField)
 
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
 
+    }
 }
+
 
